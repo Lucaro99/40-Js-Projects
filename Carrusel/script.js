ab63e1fba39799023d03f409bb858d6c55a71ab0
Lucaro99 <rcata99@gmail.com>
2024-07-04 17:02:37 -0500

    carusell

--- Carrusel/script.js
+++ Carrusel/script.js
@@ -10,7 +10,7 @@ const review = [
     id: 2,
     name: "David Salazar",
     job: "Sistem Engineer",
-    img: "/Carrusel/img/Catalina Rodriguez.jpeg",
+    img: "https://img.freepik.com/foto-gratis/retrato-hombre-negocios-al-aire-libre-sonrie_23-2148763856.jpg?t=st=1720130341~exp=1720133941~hmac=0f6624d33e6a219a64332f6fe057af117132f56fe6f1d61d67298df2cfcb4d1c&w=740",
     text: "Ejemplo 2: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsaexcepturi explicabo. Maxime excepturi quas perferendis adipisci explicabo hic, nostrum nesciunt inventore magnam voluptatibus delectus veniam, consequuntur vero soluta quod sequi.",
   },
   {
@@ -24,7 +24,7 @@ const review = [
     id: 4,
     name: "Santiago Serrano",
     job: "Abogado",
-    img: "/Carrusel/img/Catalina Rodriguez.jpeg",
+    img: "https://img.freepik.com/foto-gratis/retrato-hombre-negocios-optimista-ropa-formal_1262-3600.jpg?t=st=1720130316~exp=1720133916~hmac=17f770a836d3ed6046d0c1c78669839530e8e3f4e0ffcc609f0b43f8a3a17568&w=740",
     text: "Ejemplo 4: Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsaexcepturi explicabo. Maxime excepturi quas perferendis adipisci explicabo hic, nostrum nesciunt inventore magnam voluptatibus delectus veniam, consequuntur vero soluta quod sequi.",
   },
 ];
