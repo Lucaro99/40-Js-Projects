eda48f7e4257d9c8102b21aa618935e9e9e61665
Lucaro99 <rcata99@gmail.com>
2024-07-08 12:29:10 -0500

    navbar1

--- NavBar/scripts.js
+++ NavBar/scripts.js
@@ -1,3 +1,11 @@
 document.getElementById("nav-toggle").addEventListener('click', ()=>{
     document.getElementById('links').classList.toggle('hide-toggle');
+    const icon = document.getElementById('icon');
+    if (icon.classList.contains('fa-bars')){
+        icon.classList.remove('fa-bars');
+        icon.classList.add('fa-xmark');
+    } else {
+        icon.classList.remove('fa-xmark');
+        icon.classList.add('fa-bars');
+    }
 });
